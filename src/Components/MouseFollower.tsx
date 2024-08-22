c49825bec757dfcaa2225313c4772b88971d8fb5
sushant-wayal <sushantssr12345@gmail.com>
2024-08-23 01:19:33 +0530

    added mouse follower border

--- src/Components/MouseFollower.tsx
+++ src/Components/MouseFollower.tsx
@@ -1,3 +1,4 @@
+import gsap from "gsap";
 import { useEffect, useRef } from "react";
 
 interface MouseFollowerProps {
@@ -7,13 +8,29 @@ interface MouseFollowerProps {
 
 export const MouseFollower: React.FC<MouseFollowerProps> = ({ clientX, clientY }) => {
   const mouseFollowerRef = useRef<HTMLDivElement>(null);
+  const mouseFollowerBorderRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (mouseFollowerRef.current) {
-      mouseFollowerRef.current.style.left = `${clientX}px`;
-      mouseFollowerRef.current.style.top = `${clientY}px`;
+      gsap.to(mouseFollowerRef.current, {
+        duration: 0,
+        x: clientX,
+        y: clientY,
+        ease: "power4.out"
+      });
     }
-  }, [clientX, clientY, mouseFollowerRef]);
+    if (mouseFollowerBorderRef.current) {
+      gsap.to(mouseFollowerBorderRef.current, {
+        duration: 1,
+        x: clientX-6,
+        y: clientY-6,
+        ease: "power4.out"
+      });
+    }
+  }, [clientX, clientY, mouseFollowerRef, mouseFollowerBorderRef]);
   return (
-    <div className="bg-red-600 rounded-full h-10 w-10 fixed z-[9999]" ref={mouseFollowerRef}></div>
+    <>
+      <div className="bg-white rounded-full h-5 w-5 fixed z-[9999]" ref={mouseFollowerRef}></div>
+      <div className="bg-transparent rounded-full border-2 h-8 w-8 fixed z-[9999] border-white" ref={mouseFollowerBorderRef}></div>
+    </>
   );
 };

dc4a39930e7af5118987914a7ebb14e78cdb8cfd
sushant-wayal <sushantssr12345@gmail.com>
2024-10-02 12:27:47 +0530

    made projects page responsive

--- src/Components/pages/Projects.tsx
+++ src/Components/pages/Projects.tsx
@@ -2,15 +2,17 @@ import { useEffect, useRef, useState } from "react";
 import { Magnetic } from "../components/Magnetic";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 gsap.registerPlugin(ScrollTrigger);
 
 interface ProjectsProps {}
 
 export const Projects: React.FC<ProjectsProps> = () => {
-  const rotatation = -8;
-  const navigate = useNavigate();
+  const [rotation, setRotation] = useState<number>(-8);
+  const breakPoint = 768;
+  // const baseWidth = 1536;
+  // const navigate = useNavigate();
   const scrollDivRef = useRef<HTMLDivElement>(null);
   const projectNameRef = useRef<HTMLDivElement>(null);
   const [projectIndex, setProjectIndex] = useState(0);
@@ -19,19 +21,35 @@ export const Projects: React.FC<ProjectsProps> = () => {
     {name: "Idea-Connect", filename: "ideaconnect.jpg", link: "ideaconnect", ref: useRef<HTMLDivElement>(null)},
     {name: "EnviScribe", filename: "enviscribe.jpg", link: "enviscribe", ref: useRef<HTMLDivElement>(null)},
     {name: "Note-Corner", filename: "notecorner.jpg", link: "notecorner", ref: useRef<HTMLDivElement>(null)},
-    {name: "Multiplayer Tic-Tac-Toe", filename: "tictactoe.jpg", link: "tictactoe", ref: useRef<HTMLDivElement>(null)},
+    {name: "Tic-Tac-Toe", filename: "tictactoe.jpg", link: "tictactoe", ref: useRef<HTMLDivElement>(null)},
   ];
   const handleScroll = () => {
+    if (window.innerWidth < breakPoint) return;
     const height = scrollDivRef.current?.clientHeight;
     const y = (height || 0)/2 - window.scrollY;
-    const x = y*Math.tan(-1*rotatation*Math.PI/180);
-    scrollDivRef.current?.style.setProperty("transform", `rotate(${rotatation}deg) translateX(${x}px)`);
+    const x = y*Math.tan(-1*rotation*Math.PI/180);
+    scrollDivRef.current?.style.setProperty("transform", `rotate(${rotation}deg) translateX(${x}px)`);
   };
   const [projectName, setProjectName] = useState("");
+  const actResponsive = () => {
+    if (window.innerWidth < breakPoint) {
+      setRotation(0);
+      gsap.to(scrollDivRef.current, {rotate: 0, duration: 0});
+    }
+    else {
+      setRotation(-8);
+      gsap.to(scrollDivRef.current, {rotate: -8, duration: 0});
+    }
+  };
   useEffect(() => {
     handleScroll();
     document.addEventListener("scroll", handleScroll);
-    return () => document.removeEventListener("scroll", handleScroll);
+    actResponsive();
+    window.addEventListener("resize", actResponsive);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", actResponsive);
+    }
   }, [scrollDivRef]);
   useEffect(() => {
     projects.forEach(({name, ref}, ind) => {
@@ -83,40 +101,41 @@ export const Projects: React.FC<ProjectsProps> = () => {
       }
     });
   }, [projects]);
-  useEffect(() => {
-    document.body.style.overflow = "auto";
-    const handleClick = (e: MouseEvent) => {
-      e.preventDefault();
-      const elements = document.elementsFromPoint(e.clientX, e.clientY);
-      elements.forEach((element) => {
-        if (element.id === "project") {
-          element.remove();
-          setDisabled(true);
-          const href = `/project/${projects[projectIndex].link}`;
-          setTimeout(() => navigate(href), 1000);
-          projects.map(({ref}, i) => {
-            if (i !== projectIndex) {
-              ref.current?.remove();
-            }
-          });
-          document.body.style.overflow = "hidden";
-          gsap.to(projects[projectIndex].ref.current, {
-            rotate: -1*rotatation,
-            scale: 1.2,
-            position: "relative",
-            top: 0.5*window.innerHeight+window.scrollY-projectIndex*((projects[projectIndex].ref.current?.clientHeight || 0)+86.25),
-            left: -0.05375*window.innerWidth,
-            duration: 1,
-          });
-        }
-      });
-    };
-    document.addEventListener("click", handleClick);
-    return () => document.removeEventListener("click", handleClick);
-  },[projectIndex, projects, projectName]);
+  // useEffect(() => {
+  //   document.body.style.overflow = "auto";
+  //   const handleClick = (e: MouseEvent) => {
+  //     e.preventDefault();
+  //     const elements = document.elementsFromPoint(e.clientX, e.clientY);
+  //     elements.forEach((element) => {
+  //       if (element.id === "project") {
+  //         element.remove();
+  //         setDisabled(true);
+  //         const href = `/project/${projects[projectIndex].link}`;
+  //         setTimeout(() => navigate(href), 1000);
+  //         projects.map(({ref}, i) => {
+  //           if (i !== projectIndex) {
+  //             ref.current?.remove();
+  //           }
+  //         });
+  //         document.body.style.overflow = "hidden";
+  //         gsap.to(projects[projectIndex].ref.current, {
+  //           rotate: -1*rotation,
+  //           scale: 1.2,
+  //           position: "relative",
+  //           top: 0.5*window.innerHeight+window.scrollY-projectIndex*((projects[projectIndex].ref.current?.clientHeight || 0)+86.25),
+  //           // top: 0.5*window.innerHeight+window.scrollY+(baseWidth/window.innerWidth)*20-projectIndex*((projects[projectIndex].ref.current?.clientHeight || 0)+100)*Math.cos(rotation*Math.PI/180),
+  //           left: window.innerWidth >= breakPoint ? -0.05375*window.innerWidth : 0,
+  //           duration: 1,
+  //         });
+  //       }
+  //     });
+  //   };
+  //   document.addEventListener("click", handleClick);
+  //   return () => document.removeEventListener("click", handleClick);
+  // },[projectIndex, projects, projectName]);
   return (
     <div className="w-lvw flex justify-center overflow-x-hidden">
-      <div className="w-3/5 mt-[12%]">
+      <div className="w-3/5  mt-[30%] sm:mt-[12%]">
         <div className="w-full flex flex-col gap-20" ref={scrollDivRef}>
           {projects.map(({filename, ref}, i) => (
             <Magnetic key={i} color="" range={250} height="70vh" width="60vw" strength={0.2} scale={1} className="flex justify-center items-center" disabled={disabled}>
@@ -125,11 +144,11 @@ export const Projects: React.FC<ProjectsProps> = () => {
             </Magnetic>
           ))}
         </div>
-        <div id="project" className="fixed bottom-[10%] right-[15%] z-[10000]">
-          <Magnetic color="black" range={100} height="150px" strength={1.5} className="flex justify-center items-center rounded-full text-white">
+        <Link id="project" className="fixed bottom-1/2 right-1/2 translate-x-1/2 translate-y-[150%] md:bottom-[10%] md:right-[15%] md:translate-x-0 md:translate-y-0 z-[10000]" to={`/project/${projects[projectIndex].link}`}>
+          <Magnetic color="black" range={100} height={window.innerWidth >= breakPoint ? "150px" : "100px"} strength={1.5} className="flex justify-center items-center rounded-full text-white">
             <p className="text-5xl">+</p>
           </Magnetic>
-        </div>
+        </Link>
         <div className="w-full h-32"></div>
         <p className="fixed top-1/2 w-full left-0 text-6xl z-[10000] font-semibold text-center text-white" ref={projectNameRef}>
           {projectName}
